0d58c43cfb858acc79fd50d2b07a547c0de8ead1
DevDyna <andrearisorti2003@gmail.com>
2025-11-29 16:03:52 +0100

    fixed insta-break when allowChiselDamage = false

--- src/main/java/com/leclowndu93150/chisel/api/IChiselItem.java
+++ src/main/java/com/leclowndu93150/chisel/api/IChiselItem.java
@@ -1,5 +1,6 @@
 package com.leclowndu93150.chisel.api;
 
+import com.leclowndu93150.chisel.ChiselConfig;
 import com.leclowndu93150.chisel.api.carving.IChiselMode;
 import net.minecraft.core.BlockPos;
 import net.minecraft.sounds.SoundEvent;
@@ -98,7 +99,7 @@ public interface IChiselItem {
 
         int toCraft = Math.min(source.getCount(), target.getMaxStackSize());
 
-        if (chisel.isDamageableItem()) {
+        if (chisel.isDamageableItem() && ChiselConfig.allowChiselDamage) {
             int durabilityLeft = chisel.getMaxDamage() - chisel.getDamageValue() + 1;
             toCraft = Math.min(toCraft, durabilityLeft);
             chisel.hurtAndBreak(toCraft, player, slot);
